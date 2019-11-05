42e5db0274aab873862f026c62bcd4d591937cd1
dev1980 <brham1980@gmail.com>
2019-11-06 04:38:30 +0545

    create and index action completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
+  before_action :user_only, only: [:new, :create]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to root_path
+    else
+      render html: "Post is not saved"
+    end
   end
 
   def index
+    @posts = Post.all
   end
+
+  private
+
+  def user_only
+    redirect_to login_path unless current_user
+  end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+      
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/new'
-  get 'posts/create'
+  post 'posts/create'
+  root to: 'posts#index'
   get 'posts/index'
-  root to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
